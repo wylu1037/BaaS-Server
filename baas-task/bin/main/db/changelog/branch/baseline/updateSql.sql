07dc96c0828e78e6b288a0b62c55563ea0a9de2c
卢文扬 <2052987369@qq.com>
2022-09-30 00:05:20 +0800

    chore: 删除bin文件夹

--- baas-task/bin/main/db/changelog/branch/baseline/updateSql.sql
+++
@@ -1,9 +0,0 @@
---liquibase formatted sql
-
---changeset wylu:202204152312022 job_entity表增加字段groupName
-alter table job_entity
-    add column groupName varchar(50) null comment '分组名称' after name;
-
---changeset wylu:20220416121103732 job_entity表增加字段operatorId
-alter table job_entity
-    add column operatorId int null comment '操作人员id' after groupName;
